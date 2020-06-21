d88b3c30f373eee5e1100bd578223ed145acb163
Fernando <fernando.eb@icloud.com>
2020-06-20 23:49:20 -0400

    primeras pantallas de alertas

---
+++ formation/formation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  formation
+//
+//  Created by Fernando on 19-06-20.
+//  Copyright © 2020 copycat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
