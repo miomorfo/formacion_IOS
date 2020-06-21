46dcc1e93edd92481af092d0b3208da92d86eb6c
Fernando <fernando.eb@icloud.com>
2020-06-21 14:35:37 -0400

    cambios de estilo y orden en las vistas

--- formation/formation/AlertViewController.swift
+++ formation/formation/AlertViewController.swift
@@ -33,18 +33,18 @@ class AlertViewController: UIViewController {
 		
 		//los mensajes de los actionSheet se mostrarán por consola
 		
-		//opción 1
+		//opción 1 normal
 		menu.addAction(UIAlertAction(title: "Opcion 1", style: UIAlertAction.Style.default, handler: { (alert: UIAlertAction) -> Void in
 			print("has seleccionado la opción uno")
 		}))
 		
-		//opción 2
-		menu.addAction(UIAlertAction(title: "Opcion 2", style: UIAlertAction.Style.default, handler: { (UIAlertAction) in
+		//opción 2 ,destruccivo, color rojo
+		menu.addAction(UIAlertAction(title: "Opcion 2", style: UIAlertAction.Style.destructive, handler: { (UIAlertAction) in
 			print("has seleccionado la opción dos")
 		}))
 		
-		//opción 2
-		menu.addAction(UIAlertAction(title: "Opcion 3", style: UIAlertAction.Style.default, handler: { (UIAlertAction) in
+		//opción 2, opción inferior para cancelar
+		menu.addAction(UIAlertAction(title: "Opcion 3", style: UIAlertAction.Style.cancel, handler: { (UIAlertAction) in
 			print("has seleccionado la opción tres")
 		}))
 		
